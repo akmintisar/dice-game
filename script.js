ec3cda280624ea6f0012236b6d673f1867eeca33
AKM Intisar Islam <akm.intisar@gmail.com>
2021-10-27 18:02:24 +0600

    current-score-added

--- script.js
+++ script.js
@@ -9,16 +9,25 @@ const holdingGame = document.querySelector('.btn--hold');
 const currentScoreOfPlayer1 = document.getElementById('current--0');
 const currentScoreOfPlayer2 = document.getElementById('current--1');
 
+const finalScores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 diceRoll.addEventListener('click', function () {
   let randomDiceNumberGenerator = Math.trunc(Math.random() * 6) + 1;
   document.querySelector('.dice').src = `dice-${randomDiceNumberGenerator}.png`;
+
   if (randomDiceNumberGenerator == 1) {
-    currentScoreOfPlayer2.textContent = randomDiceNumberGenerator;
+    activePlayer = 1;
+    currentScore = 0;
+    currentScore += randomDiceNumberGenerator;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    currentScoreOfPlayer1.textContent = randomDiceNumberGenerator;
+    currentScore += randomDiceNumberGenerator;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   }
 
   console.log(randomDiceNumberGenerator);
 });
-
-
